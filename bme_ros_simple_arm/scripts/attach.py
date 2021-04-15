66de0d6fa3f07304813adf261bd2a4a932b6f7bc
david.dudas <david.dudas@outlook.com>
2021-04-15 19:21:47 +0200

    End of 9th week

---
+++ bme_ros_simple_arm/scripts/attach.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import rospy
+from gazebo_ros_link_attacher.srv import Attach, AttachRequest, AttachResponse
+
+
+if __name__ == '__main__':
+    rospy.init_node('demo_attach_links')
+    rospy.loginfo("Creating ServiceProxy to /link_attacher_node/attach")
+    attach_srv = rospy.ServiceProxy('/link_attacher_node/attach',
+                                    Attach)
+    attach_srv.wait_for_service()
+    rospy.loginfo("Created ServiceProxy to /link_attacher_node/attach")
+
+    # Link them
+    rospy.loginfo("Attaching gripper and red box.")
+    req = AttachRequest()
+    req.model_name_1 = "mogi_arm"
+    req.link_name_1 = "left_finger"
+    req.model_name_2 = "red_box"
+    req.link_name_2 = "link"
+
+    attach_srv.call(req)
---
+++ bme_ros_simple_arm/scripts/collision.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import rospy
+
+from gazebo_msgs.msg import ContactsState
+
+def get_contacts (msg):
+    if (len(msg.states) == 0):
+        rospy.loginfo("No contacts were detected!")
+    else:
+        if 'left_finger' in msg.states[0].collision1_name:
+            rospy.loginfo("Collision detected with %s." % msg.states[0].collision2_name.split("::")[0])
+        elif 'left_finger' in msg.states[0].collision2_name:
+            rospy.loginfo("Collision detected with %s." % msg.states[0].collision1_name.split("::")[0])
+        else:
+            rospy.loginfo("Unknown collision")
+
+
+
+rospy.init_node('contact_detector')
+
+sub_contacts = rospy.Subscriber ('/contact_vals', ContactsState, get_contacts)
+
+rospy.spin()
