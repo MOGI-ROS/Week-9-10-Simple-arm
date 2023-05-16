3b6701802d7fcb9d182c087f8243547a4b49b9c4
David Dudas <david.dudas@outlook.com>
2023-05-16 18:15:25 +0200

    add changes from starter branch

---
+++ bme_ros_simple_arm/scripts/detect_contacts.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import rospy
+
+from gazebo_msgs.msg import ContactsState
+
+def get_contacts (msg):
+
+    print(len(msg.states))
+
+    if (len(msg.states) == 0):
+        rospy.loginfo("No contacts were detected!")
+    else:
+        if 'left_finger' in msg.states[0].collision1_name:
+            rospy.loginfo("Collision detected with %s." % msg.states[0].collision2_name.split("::")[0])
+        elif 'left_finger' in msg.states[0].collision2_name:
+            rospy.loginfo("Collision detected with %s." % msg.states[0].collision1_name.split("::")[0])
+        else:
+            rospy.loginfo("Unknown collision")
+
+
+
+rospy.init_node('collision_detector')
+
+sub_contacts = rospy.Subscriber ('/contact_vals', ContactsState, get_contacts)
+
+rospy.spin()
